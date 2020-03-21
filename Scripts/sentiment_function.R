4aef62bf1a72e7d3021d76b76bbe14fb04e18976
Sofi Sinozich <sofi.sinozich@gmail.com>
2020-03-21 16:05:03 -0400

    Significantly improve speed and conciseness of sentimenter function

--- Scripts/sentiment_function.R
+++ Scripts/sentiment_function.R
@@ -9,59 +9,52 @@ data_dictionary_emoji <- dictionary(file = "Data/emoji_dictionary.txt",format="Y
 
 # By default, the function uses equation 2 on slide 11 of the 7b lecture (which is the default for analyzeSentiment)
 # (positives - negatives)/total
-sentimenter <- function(tweets,lexicoder=FALSE, equation=2, emoji=FALSE) {
-  sentiment_table <- tweets %>% select(text) %>% pull %>% analyzeSentiment(.,to='UTF-8')
+sentimenter <- function(tweets,equation=2) {
+  tweets_text <- tweets %>% select(text) %>% pull
+  tweets_tokens <- tweets_text %>% corpus %>% tokens
+  sentiment_table <- tweets_text %>% iconv(.,to="UTF-8") %>% analyzeSentiment(.)
   
-  if (lexicoder) {
-   lexicoded <- lapply(tweets$tokenized,function(x) tokens_lookup(x,dictionary = data_dictionary_LSD2015, exclusive=FALSE))
-   sentiment_table %<>% bind_cols(sentiment_map(lexicoded,1))
-   sentiment_table %<>% mutate(SentimentLC = (lcpos-lcneg)/WordCount, NegativityLC = lcneg/WordCount, PositivityLC = lcpos/WordCount)
-  }
+  lexicoded <- tweets_tokens %>% tokens_lookup(dictionary = data_dictionary_LSD2015, exclusive=FALSE)
+  sentiment_table %<>% bind_cols(sentiment_map(lexicoded,1)) %>% 
+    mutate(SentimentLC = (lcpos-lcneg)/WordCount, NegativityLC = lcneg/WordCount, PositivityLC = lcpos/WordCount) %>% 
+    select(-lcpos,-lcneg)
   
-  if (emoji) {
-    emojified <- lapply(tweets$tokenized, function(x) tokens_lookup(x,dictionary = data_dictionary_emoji, exclusive=FALSE))
-    sentiment_table %<>% bind_cols(sentiment_map(emojified,2))
-    sentiment_table %<>% mutate(SentimentEmoji = (epos-eneg)/WordCount, NegativityEmoji = eneg/WordCount, PositivityEmoji = epos/WordCount)
-  }
-  
+  emoji <- tweets_tokens %>% tokens_lookup(dictionary = data_dictionary_emoji, exclusive=FALSE)
+  sentiment_table %<>% bind_cols(sentiment_map(lexicoded,2)) %>% 
+    mutate(SentimentEmoji = (epos-eneg)/WordCount, NegativityEmoji = eneg/WordCount, PositivityEmoji = epos/WordCount) %>% 
+    select(-epos,-eneg)
+
+ 
   # Use equation 1 instead
   # positives/negatives
   if (equation == 1) {
-    sentiment_table %<>% mutate(SentimentGI = PositivityGI/NegativityGI, 
+    sentiment_table %<>% mutate(SentimentGI = PositivityGI/NegativityGI,
                                 SentimentHE = PositivityHE/NegativityHE,
                                 SentimentLM = PositivityLM/NegativityLM,
-                                SentimentQDAP = PositivityQDAP/NegativityQDAP)
-    if (lexicoder) {
-      sentiment_table %<>% mutate(SentimentLC = PositivityLC/NegativityLC)
-    }
-    if (emoji) {
-      sentiment_table %<>% mutate(SentimentEmoji = PositivityEmoji/NegativityEmoji)
-    }
+                                SentimentQDAP = PositivityQDAP/NegativityQDAP,
+                                SentimentLC = PositivityLC/NegativityLC,
+                                SentimentEmoji = PositivityEmoji/NegativityEmoji)
   }
-  
+ 
   # Use equation 3 instead
   # positives/(positives + negatives)
   if (equation == 3) {
-    sentiment_table %<>% mutate(SentimentGI = PositivityGI/(PositivityGI+NegativityGI), 
+    sentiment_table %<>% mutate(SentimentGI = PositivityGI/(PositivityGI+NegativityGI),
                                 SentimentHE = PositivityHE/(PositivityHE+NegativityHE),
                                 SentimentLM = PositivityLM/(PositivityLM+NegativityLM),
-                                SentimentQDAP = PositivityQDAP/(PositivityQDAP+NegativityQDAP))
-    if (lexicoder) {
-      sentiment_table %<>% mutate(SentimentLC = PositivityLC/(PositivityLC+NegativityLC))
-    }
-    if (emoji) {
-      sentiment_table %<>% mutate(SentimentEmoji = PositivityEmoji/(PositivityEmoji+NegativityEmoji))
-    }
+                                SentimentQDAP = PositivityQDAP/(PositivityQDAP+NegativityQDAP),
+                                SentimentLC = PositivityLC/(PositivityLC+NegativityLC),
+                                SentimentEmoji = PositivityEmoji/(PositivityEmoji+NegativityEmoji))
   }
-  
+
   return(sentiment_table)
 }
 
 sentiment_map <- function(lookedup,use) {
-  pos <- map_int(lookedup,function(x) sum(x %>% pluck(1) == "POSITIVE"))
-  neg <- map_int(lookedup,function(x) sum(x %>% pluck(1) == "NEGATIVE"))
-  negpos <- map_int(lookedup,function(x) sum(x %>% pluck(1) == "NEG_POSITIVE"))
-  negneg <-map_int(lookedup,function(x) sum(x %>% pluck(1) == "NEG_NEGATIVE"))
+  pos <- map_int(lookedup,function(x) sum(x == "POSITIVE"))
+  neg <- map_int(lookedup,function(x) sum(x == "NEGATIVE"))
+  negpos <- map_int(lookedup,function(x) sum(x == "NEG_POSITIVE"))
+  negneg <-map_int(lookedup,function(x) sum(x == "NEG_NEGATIVE"))
   totalpos <- (pos-negpos+negneg)
   totalneg <- (neg-negneg+negpos)
   if (use == 1) {
