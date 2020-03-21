4f825502e31a8973a98a6fa734cc8e8e4f7a77c1
Sofi Sinozich <sofi.sinozich@gmail.com>
2020-03-21 17:21:06 -0400

    Updated sentiment function to remove stopwords, tweaks to calculation

--- Scripts/sentiment_function.R
+++ Scripts/sentiment_function.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(magrittr)
 
 data_dictionary_emoji <- dictionary(file = "Data/emoji_dictionary.txt",format="YAML",tolower=FALSE)
+data_dictionary_lcemoji <- dictionary(c(as.list(data_dictionary_emoji),as.list(data_dictionary_LSD2015)))
 
 # Sample use: tweets %>% bind_cols(sentimenter(.))
 
@@ -11,18 +12,28 @@ data_dictionary_emoji <- dictionary(file = "Data/emoji_dictionary.txt",format="Y
 # (positives - negatives)/total
 sentimenter <- function(tweets,equation=2) {
   tweets_text <- tweets %>% select(text) %>% pull
-  tweets_tokens <- tweets_text %>% corpus %>% tokens
+  tweets_tokens <- tweets_text %>% corpus %>% 
+    tokens(remove_punct = TRUE, remove_symbols=TRUE,remove_url=TRUE,remove_separators=TRUE) %>% 
+    tokens_remove(stopwords("english")) %>% # Remove English stopwords
+    tokens_remove("^[A-z]{1}$",valuetype="regex") # Remove single-letter words
+  tokens_length <- tweets_tokens %>% pluck(1) %>% length
   sentiment_table <- tweets_text %>% iconv(.,to="UTF-8") %>% analyzeSentiment(.)
   
   lexicoded <- tweets_tokens %>% tokens_lookup(dictionary = data_dictionary_LSD2015, exclusive=FALSE)
   sentiment_table %<>% bind_cols(sentiment_map(lexicoded,1)) %>% 
-    mutate(SentimentLC = (lcpos-lcneg)/WordCount, NegativityLC = lcneg/WordCount, PositivityLC = lcpos/WordCount) %>% 
+    mutate(SentimentLC = (lcpos-lcneg)/tokens_length, NegativityLC = lcneg/tokens_length, PositivityLC = lcpos/tokens_length) %>% 
     select(-lcpos,-lcneg)
   
   emoji <- tweets_tokens %>% tokens_lookup(dictionary = data_dictionary_emoji, exclusive=FALSE)
   sentiment_table %<>% bind_cols(sentiment_map(emoji,2)) %>% 
-    mutate(SentimentEmoji = (epos-eneg)/WordCount, NegativityEmoji = eneg/WordCount, PositivityEmoji = epos/WordCount) %>% 
+    mutate(SentimentEmoji = (epos-eneg)/tokens_length, NegativityEmoji = eneg/tokens_length, PositivityEmoji = epos/tokens_length) %>% 
     select(-epos,-eneg)
+  
+  lexicoded_emoji <- tweets_tokens %>% tokens_lookup(dictionary = data_dictionary_lcemoji,exclusive=FALSE)
+  sentiment_table %<>% bind_cols(sentiment_map(lexicoded_emoji,3)) %>%
+    mutate(SentimentLCE = (lcepos-lceneg)/tokens_length, NegativityLCE = lceneg/tokens_length, PositivityLCE = lcepos/tokens_length) %>% 
+    select(-lcepos,-lceneg)
+    
 
  
   # Use equation 1 instead
@@ -33,7 +44,8 @@ sentimenter <- function(tweets,equation=2) {
                                 SentimentLM = PositivityLM/NegativityLM,
                                 SentimentQDAP = PositivityQDAP/NegativityQDAP,
                                 SentimentLC = PositivityLC/NegativityLC,
-                                SentimentEmoji = PositivityEmoji/NegativityEmoji)
+                                SentimentEmoji = PositivityEmoji/NegativityEmoji,
+                                SentimentLCE = PositivityLCE/NegativityLCE)
   }
  
   # Use equation 3 instead
@@ -44,7 +56,8 @@ sentimenter <- function(tweets,equation=2) {
                                 SentimentLM = PositivityLM/(PositivityLM+NegativityLM),
                                 SentimentQDAP = PositivityQDAP/(PositivityQDAP+NegativityQDAP),
                                 SentimentLC = PositivityLC/(PositivityLC+NegativityLC),
-                                SentimentEmoji = PositivityEmoji/(PositivityEmoji+NegativityEmoji))
+                                SentimentEmoji = PositivityEmoji/(PositivityEmoji+NegativityEmoji),
+                                SentimentLCE = PositivityLCE/(PositivityLCE+NegativityLCE))
   }
 
   return(sentiment_table)
@@ -60,7 +73,10 @@ sentiment_map <- function(lookedup,use) {
   if (use == 1) {
     return(bind_cols(lcpos=totalpos,lcneg=totalneg))
   }
-  else {
+  else if (use == 2) {
     return(bind_cols(epos=totalpos,eneg=totalneg))
+  }
+  else if (use == 3) {
+    return(bind_cols(lcepos=totalpos,lceneg=totalneg))
   }
 }
