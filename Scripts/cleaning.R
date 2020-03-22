1110df905e870e1f7166dd425da92447d1ab760e
Sofi Sinozich <sofi.sinozich@gmail.com>
2020-03-22 14:03:43 -0400

    Data folder cleanup

--- Scripts/cleaning.R
+++ Scripts/cleaning.R
@@ -10,12 +10,12 @@ source("Scripts/load_dataviz_themes.R")
 
 # Don't do this again unless something is wrong with the tokenized tweets!
 # Concatenate all the streamed tweets
-# ss1_tweets <- parse_stream("Data/ss_streamed_tweets.json")
-# bs_tweets <- parse_stream("Data/bs_streamed_tweets_mar5-10am-mar8-1211pm.json")
-# rj1_tweets <- parse_stream("Data/ss_streamed_tweets_RJ2.json")
-# rj2_tweets <- parse_stream("Data/ss_streamed_tweets_RJ3.json")
-# rj3_tweets <- parse_stream("Data/ss_streamed_tweets_RJ4.json")
-# ss2_tweets <- parse_stream("Data/ss_mar8-mar13_tweets.json")
+# ss1_tweets <- parse_stream("Data/streamed/ss_streamed_tweets.json")
+# bs_tweets <- parse_stream("Data/streamed/bs_streamed_tweets_mar5-10am-mar8-1211pm.json")
+# rj1_tweets <- parse_stream("Data/streamed/ss_streamed_tweets_RJ2.json")
+# rj2_tweets <- parse_stream("Data/streamed/ss_streamed_tweets_RJ3.json")
+# rj3_tweets <- parse_stream("Data/streamed/ss_streamed_tweets_RJ4.json")
+# ss2_tweets <- parse_stream("Data/streamed/ss_mar8-mar13_tweets.json")
 # 
 # 
 # maryland_tweets <- bind_rows(ss1_tweets,bs_tweets,rj1_tweets,rj2_tweets,rj3_tweets,ss2_tweets,.id="session") %>% distinct(status_id,.keep_all = TRUE)
