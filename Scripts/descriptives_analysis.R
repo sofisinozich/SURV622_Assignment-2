35ef1d240cd18928764018236515205bc3c500da
Ben Schneider <benjamin.julius.schneider@gmail.com>
2020-03-22 15:30:07 -0400

    Add main 'tweets by time' plot. Update data viz themes.

--- Scripts/descriptives_analysis.R
+++ Scripts/descriptives_analysis.R
@@ -97,3 +97,190 @@ plot(frequent_terms_clean2)
       x = "Time of Day",
       y = "# of tweets\ncreated"
     )
+  
+  #_ Tweets by half-hour increments on key dates ----
+  
+    tweets_by_time <- relevant_tweets %>%
+      mutate(
+        week_index = case_when(
+          month(created_at_eastern) == 2 ~ "Week 1",
+          month(created_at_eastern) == 3 & day(created_at_eastern) <= 7 ~ "Week 2",
+          TRUE ~ "Week 3"
+        ),
+        date = paste0(month(created_at_eastern, label = TRUE), " ", day(created_at_eastern)),
+        day_of_week = wday(created_at_eastern, label = TRUE, abbr = FALSE) %>%
+          as.character(),
+        is_game_day = case_when(
+          as.character(date(created_at_eastern)) %in% c("2020-02-29", "2020-03-03", "2020-03-08") ~ "Game day",
+          TRUE ~ "Non-game day"
+        ),
+        half_hour = paste0(hour(created_at_eastern),
+                           ifelse(minute(created_at_eastern) < 30, ":00", ":30")
+        ) %>%
+          factor(levels = map(0:23, ~ paste0(.x, c(":00", ":30"))) %>% unlist())
+      )
+  
+  ##__ Day of Big Ten cancellation
+    tweets_in_announcement_block <- tweets_by_time %>%
+      filter(date == "Mar 12") %>%
+      count(half_hour, name = 'Tweets', .drop = FALSE) %>%
+      filter(half_hour == "11:30") %>% pull("Tweets")
+    
+    tweets_in_preceding_block <- tweets_by_time %>%
+      filter(date == "Mar 12") %>%
+      count(half_hour, name = 'Tweets', .drop = FALSE) %>%
+      filter(half_hour == "11:00") %>% pull("Tweets")
+  
+    tweets_by_time %>%
+      filter(date == "Mar 12") %>%
+      count(half_hour, name = 'Tweets', .drop = FALSE) %>%
+      ggplot(aes(x = half_hour, y = Tweets)) +
+      geom_col(color = 'white') +
+      annotate(geom = 'text', y = 95, x = "11:30",
+               label = "Big Ten Tournament\nCancellation Announced\nat 11:45 am\n↓") +
+      annotate(geom = 'text', y = 30, x = "19:00",
+               label = "      Apparent\n      streaming\n      disconnect", size = 3) +
+      coord_cartesian(expand = c(0,0)) +
+      scale_x_discrete(name = "Time of Day (in half hour increments)",
+                       labels = function(breaks) 
+        case_when(endsWith(breaks, "00") ~  paste0(str_extract(breaks, ".{1,2}(?=\\:)"),
+                                                   "\n",
+                                                   str_extract(breaks, "(?<=\\:).{1,2}")),
+                  TRUE ~ paste0("\n",
+                                str_extract(breaks, "(?<=\\:).{1,2}"))
+        )
+      ) +
+      labs(
+        title = paste(c("When the Big Ten Conference announced the tournament's cancellation at 11:45am on March 12,",
+                      glue::glue("the number of tweets increased by {100*((tweets_in_announcement_block/tweets_in_preceding_block)-1)}%.")),
+                      collapse = "\n")
+      )
+    
+    
+  # Overview plot: day of week and time of day with annotations
+    tweets_by_time %>%
+      count(is_game_day, date, week_index, day_of_week, half_hour, name = 'Tweets', .drop = FALSE) %>%
+      mutate(date = paste0(date, "\n", day_of_week) %>% fct_reorder(as.numeric(date)),
+             day_of_week = factor(day_of_week, c("Sunday", "Monday", "Tuesday", "Wednesday",
+                                                 "Thursday", "Friday", "Saturday"))) %>%
+      ggplot(aes(x = half_hour, y = Tweets)) +
+      lemon::facet_rep_grid(week_index ~ day_of_week, repeat.tick.labels = 'x',
+                            switch = 'y', drop = FALSE) +
+      geom_col(aes(fill = is_game_day, color = is_game_day)) +
+      # Grey out entire days without data collection
+      geom_rect(data = tibble(
+        day_of_week = factor(c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
+                               "Monday", "Tuesday", "Wednesday", "Wednesday", "Saturday"),
+                             c("Sunday", "Monday", "Tuesday", "Wednesday",
+                               "Thursday", "Friday", "Saturday")),
+        is_game_day = c("Game day", "Non-game day", "Non-game day", "Game day", "Non-game day", "Non-game day",
+                        "Non-game day", "Game day", "Non-game day", "Non-game day", "Non-game day"),
+        week_index = c("Week 1", "Week 1", "Week 1", "Week 1", "Week 1", "Week 1",
+                       "Week 2", "Week 2", "Week 2", "Week 3", "Week 3"),
+        half_hour = factor(rep("0:00", times = 11),
+                           levels(tweets_by_time$half_hour)),
+        Tweets = rep(NA_integer_, times = 11)
+      ), xmin = factor("0:00", levels(tweets_by_time$half_hour)), xmax = factor("23:30", levels(tweets_by_time$half_hour)),
+      ymin = -Inf, ymax = Inf,
+      fill = 'lightgray', alpha = 0.75) +
+      # Grey out specific time ranges with streaming disconnects or other non-coverage
+        #_ February 29: 0:00 through 16:30
+        geom_rect(data = tibble(
+          day_of_week = factor(c("Saturday"),
+                               c("Sunday", "Monday", "Tuesday", "Wednesday",
+                                 "Thursday", "Friday", "Saturday")),
+          is_game_day = c("Game day"),
+          week_index = c("Week 1"),
+          half_hour = factor(rep("0:00", times = 11),
+                             levels(tweets_by_time$half_hour)),
+          Tweets = rep(NA_integer_)
+        ),
+        xmin = factor("0:00", levels(tweets_by_time$half_hour)),
+        xmax = factor("16:30", levels(tweets_by_time$half_hour)),
+        ymin = -Inf, ymax = Inf,
+        fill = 'lightgray', alpha = 0.75) +
+        #_ March 9: 9:00 through 22:30
+        geom_rect(data = tibble(
+          day_of_week = factor(c("Monday"),
+                               c("Sunday", "Monday", "Tuesday", "Wednesday",
+                                 "Thursday", "Friday", "Saturday")),
+          is_game_day = c("Non-game day"),
+          week_index = c("Week 3"),
+          half_hour = factor(rep("0:00", times = 11),
+                             levels(tweets_by_time$half_hour)),
+          Tweets = rep(NA_integer_)
+        ),
+        xmin = factor("9:00", levels(tweets_by_time$half_hour)),
+        xmax = factor("22:30", levels(tweets_by_time$half_hour)),
+        ymin = -Inf, ymax = Inf,
+        fill = 'lightgray', alpha = 0.75) +
+        #_ March 12: 18:30 through 20:00
+        geom_rect(data = tibble(
+          day_of_week = factor(c("Thursday"),
+                               c("Sunday", "Monday", "Tuesday", "Wednesday",
+                                 "Thursday", "Friday", "Saturday")),
+          is_game_day = c("Non-game day"),
+          week_index = c("Week 3"),
+          half_hour = factor(rep("0:00", times = 11),
+                             levels(tweets_by_time$half_hour)),
+          Tweets = rep(NA_integer_)
+        ),
+        xmin = factor("18:30", levels(tweets_by_time$half_hour)),
+        xmax = factor("20:00", levels(tweets_by_time$half_hour)),
+        ymin = -Inf, ymax = Inf,
+        fill = 'lightgray', alpha = 0.75) +
+        #_ March 13: 13:00 through 23:30
+        geom_rect(data = tibble(
+          day_of_week = factor(c("Friday"),
+                               c("Sunday", "Monday", "Tuesday", "Wednesday",
+                                 "Thursday", "Friday", "Saturday")),
+          is_game_day = c("Non-game day"),
+          week_index = c("Week 3"),
+          half_hour = factor(rep("0:00", times = 11),
+                             levels(tweets_by_time$half_hour)),
+          Tweets = rep(NA_integer_)
+        ),
+        xmin = factor("13:00", levels(tweets_by_time$half_hour)),
+        xmax = factor("23:30", levels(tweets_by_time$half_hour)),
+        ymin = -Inf, ymax = Inf,
+        fill = 'lightgray', alpha = 0.75) +
+      # Text annotations
+        #_ First game (Feb 29)
+        geom_text(data = tibble(
+          half_hour = "12:00",
+          day_of_week = factor("Saturday", c("Sunday", "Monday", "Tuesday", "Wednesday",
+                                             "Thursday", "Friday", "Saturday")),
+          is_game_day = "Game day",
+          week_index = "Week 1",
+        ), y = 300, label = "Michigan State\nStarts at 20:00") +
+        #_ Second game (Mar 8)
+        geom_text(data = tibble(
+          half_hour = "6:30",
+          day_of_week = factor("Sunday", c("Sunday", "Monday", "Tuesday", "Wednesday",
+                                           "Thursday", "Friday", "Saturday")),
+          is_game_day = "Game day",
+          week_index = "Week 3",
+        ), y = 450, label = "Michigan\nStarts at noon") +
+        #_ Tournament cancellation
+        geom_text(data = tibble(
+          half_hour = "11:30",
+          day_of_week = factor("Thursday", c("Sunday", "Monday", "Tuesday", "Wednesday",
+                                             "Thursday", "Friday", "Saturday")),
+          is_game_day = "Non-game day",
+          week_index = "Week 3",
+        ), y = 300, label = "Big Ten\nTournament\nCancelled\n↓") +
+      # Add scales and titles
+      scale_x_discrete(name = "Time of Day (in half hour increments)",
+                       breaks = levels(tweets_by_time$half_hour),
+                       labels = function(breaks) ifelse(endsWith(breaks, ":00") &
+                                                        str_detect(breaks, "^(0|8|12|16|20)"),
+                                                        str_extract(breaks, ".{1,2}(?=\\:)"),
+                                                        "")) +
+      scale_fill_manual(name = "Type of day",
+                        values = c("Game day" = .maryland_red, "Non-game day" = 'gray34')) +
+      scale_color_manual(name = "Type of day",
+                        values = c("Game day" = .maryland_red, "Non-game day" = 'gray34')) +
+      labs(
+        subtitle = "Number of tweets by day and time of day",
+        title = "The volume of tweets is vastly larger on game days.\nEven on the day of the Big Ten Tournament's cancellation, the volume of tweets is nowhere as large as on a game day."
+      )
